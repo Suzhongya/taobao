7fa200ec7ad4bfaf6acea14999cad3cb2d65d9a9
lexed <lexed@BD-02-40>
2020-10-12 18:54:33 +0800

    第一次

---
+++ src/main/java/top/taobao/TaobaoApp.java
@@ -0,0 +1,11 @@
+package top.taobao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaobaoApp {
+	public static void main(String[] args) {
+		SpringApplication.run(TaobaoApp.class, args);
+	}
+}
